822e4a5946c6e7f8b9c6eb8093b840b507bca247
Robert Komar <robkomar98@gmail.com>
2019-05-29 22:20:34 +0200

    v0.1.5

--- headers/automative.h
+++ headers/automative.h
@@ -1,6 +1,7 @@
 #ifndef PENGUINS_V0_1_AUTOMATIVE_H
 #define PENGUINS_V0_1_AUTOMATIVE_H
 
-int automative(GameState gameState, char *phase, int dimx, int dimy, int nPlayer, int nPenguin);
-int AutoPlacement(GameState gameState, int dimx, int dimy, int nPlayer, int nPenguin);
+int automative(GameState gameState, char *phase, int dimx, int dimy, int nPlayers, int nPenguins, int ourPlayer);
+int AutoPlacement(GameState gameState, int dimx, int dimy, int nPlayer, int nPenguins);
+int AutoMovement(GameState gameState, int dimx, int dimy, int nPlayers, int playerNumber, int nPenguins);
 #endif
--- sources/automative.c
+++ sources/automative.c
@@ -1,14 +1,16 @@
 #include "../headers/automative.h"
 
-int automative(GameState gameState, char *phase, int dimx, int dimy, int nPlayer, int nPenguin){
+int automative(GameState gameState, char *phase, int dimx, int dimy, int nPlayers, int nPenguins, int ourPlayer){
 
-    // MAMY SE WCZYTANA MAPE HERE
     //WIEC TERA TA DZIELIMY SE NA PLACEMENT PHASE i MOVEMENT PHASE
     if(strcmp(phase, "placement") == 0)
     {
-        AutoPlacement(gameState);
+        AutoPlacement(gameState, dimx, dimy, ourPlayer, nPenguins);
+       // saveGame(gameState, dimx, dimy, gameState.map, ourPlayer);
     }else if(strcmp(phase, "manual")== 0){
-        AutoMovement(Gamestate);
+        AutoMovement(gameState, dimx, dimy, nPlayers, ourPlayer, nPenguins);
+       // saveGame(gameState, dimx, dimy, gameState.map, ourPlayer);
+
     }
     //GITTARA
 
@@ -17,21 +19,37 @@ int automative(GameState gameState, char *phase, int dimx, int dimy, int nPlayer
 
 // AUTPLACEMENT FUNKCJA XD
 
-int AutoPlacement(GameState gameState, int dimx, int dimy, int nPlayer, int nPenguin){
+int AutoPlacement(GameState gameState, int dimx, int dimy, int nPlayer, int nPenguins){
 
     int targetX=0, targetY=0, areProperValuesUsed;
-    //SRAND trzeba targetX i targetY i
     do {
-        //TODO: WE TU SRANDA WSTAW W JAKIMS ZAKRESIE DIMX DIMY DLA TARGETX I TARGETY
-        areProperValuesUsed = placePenguin(dimx, dimy, targetX, targetY, gameState.map, gameState.players, nPlayer, nPenguin);
+        targetX = rand() % dimx;
+        targetY = rand() %dimy;
+        areProperValuesUsed = placePenguin(dimx, dimy, targetX, targetY, gameState.map, gameState.players, nPlayer, nPenguins);
     }while(areProperValuesUsed);
 
-    placePenguin(dimx, dimy, targetX, targetY, gameState.map , gameState.players, nPlayer, nPenguin);
+    placePenguin(dimx, dimy, targetX, targetY, gameState.map , gameState.players, nPlayer, nPenguins);
     return 0;
 }
 
-//AUTOREGGRESION W MOZGU SE MAM
-// A I AUTOMOVEMENT FUNKCJA TEZ TU JEST XD
-int AutoMovement(){
+//AUTO
 
+int AutoMovement(GameState gameState, int dimx, int dimy, int nPlayers, int playerNumber, int nPenguins){
+
+    int choosenPenguin, penguinWithMoves, targetX, targetY, areProperValuesUsed;
+    do{
+        choosenPenguin = rand() % nPenguins;
+        penguinWithMoves = checkPeng(dimx, dimy, gameState.map, gameState.players[playerNumber].penguins[choosenPenguin]);
+    }while(penguinWithMoves);
+
+    do {
+        targetX = rand() % dimx;
+        targetY = rand() % dimy;
+        areProperValuesUsed = placePenguin(dimx, dimy, targetX, targetY, gameState.map, gameState.players, playerNumber, choosenPenguin);
+    }while(areProperValuesUsed);
+
+
+    MovePenguin(dimx, dimy, gameState.map, nPlayers, gameState.players, choosenPenguin, playerNumber, gameState.players[playerNumber].penguins[choosenPenguin].pengPositionX,gameState.players[playerNumber].penguins[choosenPenguin].pengPositionY,targetX,targetY);
+
+    return 0;
 }
