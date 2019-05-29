728c7558a68528188be6d71f4ba155c8a741868d
Robert Komar <robkomar98@gmail.com>
2019-05-29 22:47:54 +0200

    v0.1.5

--- sources/automative.c
+++ sources/automative.c
@@ -2,7 +2,6 @@
 
 int automative(GameState gameState, char *phase, int dimx, int dimy, int nPlayers, int nPenguins, int ourPlayer){
 
-    //WIEC TERA TA DZIELIMY SE NA PLACEMENT PHASE i MOVEMENT PHASE
     if(strcmp(phase, "placement") == 0)
     {
         AutoPlacement(gameState, dimx, dimy, ourPlayer, nPenguins);
@@ -12,44 +11,53 @@ int automative(GameState gameState, char *phase, int dimx, int dimy, int nPlayer
        // saveGame(gameState, dimx, dimy, gameState.map, ourPlayer);
 
     }
-    //GITTARA
 
     return 0;
 }
 
-// AUTPLACEMENT FUNKCJA XD
 
 int AutoPlacement(GameState gameState, int dimx, int dimy, int nPlayer, int nPenguins){
+    static int allPenguinsPlaced;
+    if(allPenguinsPlaced != nPenguins){
+        int targetX=0, targetY=0, areProperValuesUsed;
+        do {
+            targetX = rand() % dimx;
+            targetY = rand() %dimy;
+            areProperValuesUsed = placePenguin(dimx, dimy, targetX, targetY, gameState.map, gameState.players, nPlayer, nPenguins);
+        }while(areProperValuesUsed);
+        allPenguinsPlaced++;
 
-    int targetX=0, targetY=0, areProperValuesUsed;
-    do {
-        targetX = rand() % dimx;
-        targetY = rand() %dimy;
-        areProperValuesUsed = placePenguin(dimx, dimy, targetX, targetY, gameState.map, gameState.players, nPlayer, nPenguins);
-    }while(areProperValuesUsed);
-
-    placePenguin(dimx, dimy, targetX, targetY, gameState.map , gameState.players, nPlayer, nPenguins);
-    return 0;
+        return 0;
+    }else{
+        return 1;
+    }
 }
 
-//AUTO
 
 int AutoMovement(GameState gameState, int dimx, int dimy, int nPlayers, int playerNumber, int nPenguins){
 
-    int choosenPenguin, penguinWithMoves, targetX, targetY, areProperValuesUsed;
-    do{
-        choosenPenguin = rand() % nPenguins;
-        penguinWithMoves = checkPeng(dimx, dimy, gameState.map, gameState.players[playerNumber].penguins[choosenPenguin]);
-    }while(penguinWithMoves);
+    if(checkPlayerMoves(dimx, dimy, nPenguins, gameState.map, gameState.players[playerNumber]) ){
+        int choosenPenguin, penguinWithMoves, targetX, targetY, areProperValuesUsed;
+        do{
+            choosenPenguin = rand() % nPenguins;
+            penguinWithMoves = checkPeng(dimx, dimy, gameState.map, gameState.players[playerNumber].penguins[choosenPenguin]);
+        }while(penguinWithMoves);
 
-    do {
-        targetX = rand() % dimx;
-        targetY = rand() % dimy;
-        areProperValuesUsed = placePenguin(dimx, dimy, targetX, targetY, gameState.map, gameState.players, playerNumber, choosenPenguin);
-    }while(areProperValuesUsed);
+        do {
+            targetX = rand() % dimx;
+            targetY = rand() % dimy;
+            areProperValuesUsed = checkMoveLegal(dimx,dimy, gameState.map, gameState.players[playerNumber].penguins[choosenPenguin].pengPositionX,gameState.players[playerNumber].penguins[choosenPenguin].pengPositionY, targetX, targetY);
+        }while(!areProperValuesUsed);
 
 
-    MovePenguin(dimx, dimy, gameState.map, nPlayers, gameState.players, choosenPenguin, playerNumber, gameState.players[playerNumber].penguins[choosenPenguin].pengPositionX,gameState.players[playerNumber].penguins[choosenPenguin].pengPositionY,targetX,targetY);
+        MovePenguin(dimx, dimy, gameState.map, nPlayers, gameState.players, choosenPenguin, playerNumber, gameState.players[playerNumber].penguins[choosenPenguin].pengPositionX,gameState.players[playerNumber].penguins[choosenPenguin].pengPositionY,targetX,targetY);
 
-    return 0;
+        return 0;
+    }else{
+
+        return 1;
+
+    }
+
+
 }
